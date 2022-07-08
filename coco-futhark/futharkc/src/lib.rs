f46f74dcb95406bd4cdd8c0cdb05486e25eefde5
Leopold Luley <git@leopoldluley.de>
2022-07-08 13:10:30 +0200

    Refactor futhark build process to share more code.

---
+++ coco-futhark/futharkc/src/lib.rs
@@ -0,0 +1,66 @@
+use rerun_except::rerun_except;
+use std::{env, fs, path::PathBuf, process::Command};
+
+const CARGO_MANIFEST_DIR: &str = "CARGO_MANIFEST_DIR";
+
+fn source_dir() -> PathBuf {
+    let source = PathBuf::from("../shared/src/futhark");
+
+    assert!(source.is_dir(), "src/futhark does not exist");
+
+    source
+}
+
+fn source_file() -> PathBuf {
+    let source = source_dir().join("bbob.fut");
+
+    assert!(source.is_file(), "bbob.fut does not exist");
+
+    source
+}
+
+pub fn watch_source() {
+    let old_manifest_dir = env::var_os(CARGO_MANIFEST_DIR).unwrap();
+
+    env::set_var(CARGO_MANIFEST_DIR, source_dir().as_os_str());
+    rerun_except(&[]).expect("Failed to watch files.");
+    env::set_var(CARGO_MANIFEST_DIR, old_manifest_dir);
+}
+
+pub fn build_target(compiler: &str) {
+    let out_dir = &env::var("OUT_DIR").expect("OUT_DIR is undefined");
+    let target = &PathBuf::from(out_dir).join("futhark");
+    fs::create_dir_all(&target).expect("Could not create target dir.");
+
+    let source = &source_file();
+
+    let futhark_status = Command::new("futhark")
+        .args(&[compiler, "--library", "-o"])
+        .arg(target.join("raw"))
+        .arg(source)
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap()
+        .success();
+
+    if !futhark_status {
+        panic!("Failed to compile Futhark code");
+    }
+
+    bindgen::Builder::default()
+        .header(target.join("raw.h").to_string_lossy())
+        .allowlist_function("free")
+        .allowlist_function("futhark_.*")
+        .allowlist_type("futhark_.*")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings.")
+        .write_to_file(target.join("raw.rs"))
+        .expect("Couldn't write bindings!");
+
+    cc::Build::new()
+        .file(target.join("raw.c"))
+        .warnings(false)
+        .compile(&format!("coco-futhark-{compiler}"));
+}
--- coco-futhark/shared/build.rs
+++ coco-futhark/shared/build.rs
@@ -1,33 +1,4 @@
-fn build_target(source: &Path, target: &Path, compiler: &str) {
-    fs::create_dir_all(&target).expect("Could not create target dir.");
-
-    let futhark_status = Command::new("futhark")
-        .args(&[compiler, "--library", "-o"])
-        .arg(target.join("raw"))
-        .arg(source)
-        .spawn()
-        .unwrap()
-        .wait()
-        .unwrap()
-        .success();
-
-    if !futhark_status {
-        panic!("Failed to compile Futhark code");
-    }
-
-    bindgen::Builder::default()
-        .header(target.join("raw.h").to_string_lossy())
-        .allowlist_function("free")
-        .allowlist_function("futhark_.*")
-        .allowlist_type("futhark_.*")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-        .generate()
-        .expect("Unable to generate bindings.")
-        .write_to_file(target.join("raw.rs"))
-        .expect("Couldn't write bindings!");
-
-    cc::Build::new()
-        .file(target.join("raw.c"))
-        .warnings(false)
-        .compile(&format!("coco-futhark-{compiler}"));
+fn main() {
+    futharkc::build_target("c");
+    futharkc::watch_source();
 }
