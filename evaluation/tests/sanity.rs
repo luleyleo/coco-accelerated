a116441178e9b8db6a3dd560ec3a70073634dc01
Leopold Luley <git@leopoldluley.de>
2022-07-28 10:40:00 +0200

    Disable default features for coco-accelerated in evaluation.

--- evaluation/tests/sanity.rs
+++ evaluation/tests/sanity.rs
@@ -16,6 +16,11 @@ fn can_evaluate_futhark() {
 
     for dim in DIMENSIONS.iter().copied() {
         let mut problem = Problem::new(context, Function::Sphere, dim);
+
+        #[cfg(feature = "c")]
         let _ = problem.eval_futhark_c_single(&vec![1.0; dim]);
+
+        #[cfg(feature = "opencl")]
+        let _ = problem.eval_futhark_opencl_single(&vec![1.0; dim]);
     }
 }
