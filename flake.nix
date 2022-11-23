5ab123e36945652910872d5a615209344e5cd91d
Leopold Luley <git@leopoldluley.de>
2022-11-23 13:54:45 +0100

    Add nix flake.

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Rust + CUDA Flake";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/6d8215281b2f87a5af9ed7425a26ac575da0438f";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in {
+      devShell.x86_64-linux = pkgs.mkShell {
+        packages = [
+          pkgs.cargo
+          pkgs.futhark
+          pkgs.cudaPackages.cudatoolkit_11_2
+          pkgs.cudnn_cudatoolkit_11_2
+          pkgs.linuxPackages.nvidia_x11
+          pkgs.stdenv.cc.cc
+        ];
+      };
+    };
+}
