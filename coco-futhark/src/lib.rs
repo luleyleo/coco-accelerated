e4f4e63f66e9b05ed1b305196291d561f897e275
Leopold Luley <git@leopoldluley.de>
2023-07-24 09:39:42 +0200

    Remove note about sample library.

--- coco-futhark/src/lib.rs
+++ coco-futhark/src/lib.rs
@@ -1,9 +1,3 @@
 #![warn(missing_docs)]
 
-//! Futhark sample library.
-//!
-//! You can run `cargo doc` to get its documentation
-//! like with any other Rust library.
-//!
-
 include!(concat!(env!("OUT_DIR"), "/futhark/futhark_lib.rs"));
